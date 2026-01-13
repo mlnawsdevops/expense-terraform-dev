ae8699128503cb43b1684f47fe978f9d18ed3438
mlnawsdevops <mlnpublicact365@gmail.com>
2026-01-13 07:23:15 +0530

    expense-infra-dev

--- 30-bastion-server/main.tf
+++ 30-bastion-server/main.tf
@@ -1,11 +1,14 @@
+# open source module with aws contribution
+
+#bastion-1 server
 module "ec2_instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = local.resource_name
+  name = local.resource_name #expense-dev-bastion
 
   instance_type = "t3.micro"
-  vpc_security_group_ids = [local.bastion_sg_id]
-  subnet_id = local.bastion_subnet_ids[0]
+  vpc_security_group_ids = [local.bastion_sg_id] # bastion security group from 20-sg
+  subnet_id = local.bastion_subnet_ids[0] # us-east-1a availability zone bastion-expense-dev server
 
   tags = {
     Terraform   = "true"
@@ -14,14 +17,16 @@ module "ec2_instance" {
   }
 }
 
+
+# bastion-2 server
 module "ec2_instance1" {
   source  = "terraform-aws-modules/ec2-instance/aws"
 
-  name = "${local.resource_name}-1"
+  name = "${local.resource_name}-1" # expense-dev-bastion-1
 
   instance_type = "t3.micro"
   vpc_security_group_ids = [local.bastion_sg_id]
-  subnet_id = local.bastion_subnet_ids[1]
+  subnet_id = local.bastion_subnet_ids[1] # us-east-1b availability zone bastion-expense-dev-1 server
 
   tags = {
     Terraform   = "true"
